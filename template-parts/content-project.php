c525de98d90c9648d1711007e3a1ed72fe869e10
Bart Ledoux <ledouxb@gmail.com>
2017-07-08 15:52:26 -0500

    first batch of the next prev buttons

--- template-parts/content-project.php
+++ template-parts/content-project.php
@@ -32,4 +32,33 @@
 		the_content();
 	?>
 	</div>
+	<nav class="project-navigation">
+		<?php
+			$pagelist = get_pages(array(
+				'sort_column'=> 'menu_order',
+				'sort_order' => 'asc',
+				'parent' => wp_get_post_parent_id( get_the_ID() ),
+			));
+			$pages = array();
+			foreach ($pagelist as $page) {
+				$pages[] += $page->ID;
+			}
+
+			$current = array_search(get_the_ID(), $pages);
+			$prevID = $pages[$current-1];
+			$nextID = $pages[$current+1];
+		?>
+			<?php if (!empty($prevID)) { ?>
+			<div class="alignleft">
+				<a href="<?php echo get_permalink($prevID); ?>"
+					title="<?php echo get_the_title($prevID); ?>">&lt; Previous</a>
+			</div>
+			<?php }
+			if (!empty($nextID)) { ?>
+			<div class="alignright">
+				<a href="<?php echo get_permalink($nextID); ?>" 
+					title="<?php echo get_the_title($nextID); ?>">Next &gt;</a>
+			</div>
+			<?php } ?>
+	</nav>
 </div>
